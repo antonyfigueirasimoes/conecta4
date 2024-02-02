4725ad2624a0800c57b9393a02f1cc67242fee99
Antony Figueira <afs094@gmail.com>
2024-02-02 12:26:06 +0100

    feat: verificamos si la columna está llena

--- juego_functions.py
+++ juego_functions.py
@@ -16,7 +16,6 @@ def juega(tablero, columna, valor_ficha):
   recorrer la columna desde atras hacia delante
     al encontrar el primer cero, lo sustituyo por valor_ficha
   '''
-
   c = tablero[columna]
   indice = len(c)-1
   while indice >= 0:
@@ -25,5 +24,25 @@ def juega(tablero, columna, valor_ficha):
       break
     indice -= 1
 
+def esta_llena(tablero, columna):
+
+  '''
+  elegir la columna del tablero
+  recorrer la columna desde atras hacia adelante
+    verificamos si todas tienen una ficha o no
+  # '''
+  # res = False
+  # c = tablero[columna]
+  # indice = len(c)-1
+  # while indice >= 0:
+  #   if c[indice] == 1:
+  #     res = True
+  #   indice -=1
+
+  # return res // FUNCIONA
+  c = tablero[columna]
+  return 0 not in c #Mucho mejor. Verifica cualquier valor que quiera se usado como "ficha de jugador"
+      
+      
       
   
--- test_juego.py
+++ test_juego.py
@@ -1,4 +1,4 @@
-from juego_functions import crea_tablero, juega
+from juego_functions import crea_tablero, juega, esta_llena
 
 def test_crear_tablero():
   tab = crea_tablero(4,3)
@@ -10,3 +10,12 @@ def test_add_ficha_a_tablero():
   juega(tab, 2, 1)
 
   assert tab == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]
+
+def test_comprobar_columna_llena():
+  tab = crea_tablero(4,3)
+  juega(tab,2,'x')
+  juega(tab,2,'x')
+  juega(tab,2,'x')
+  juega(tab,2,'x')
+
+  assert esta_llena(tab, 2) == True
