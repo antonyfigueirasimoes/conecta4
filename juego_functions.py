204d93c1d13c2108806523132f434ede18a06be6
Antony Figueira <afs094@gmail.com>
2024-02-02 11:39:30 +0100

    feat: añadir ficha a columna con huecos vacios

--- juego_functions.py
+++ juego_functions.py
@@ -8,3 +8,22 @@ def crea_tablero(columnas,filas):
     matrix.append(lists)
 
   return matrix
+
+def juega(tablero, columna, valor_ficha):
+
+  '''
+  elegir la columna del tablero
+  recorrer la columna desde atras hacia delante
+    al encontrar el primer cero, lo sustituyo por valor_ficha
+  '''
+
+  c = tablero[columna]
+  indice = len(c)-1
+  while indice >= 0:
+    if c[indice] == 0:
+      c[indice] = valor_ficha
+      break
+    indice -= 1
+
+      
+  
